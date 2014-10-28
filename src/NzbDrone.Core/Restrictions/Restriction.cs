93343a394057eaf5423373d3ab017261271304d1
Mark McDowall <markus.mcd5@gmail.com>
2014-10-27 21:37:35 -0700

    WIP

---
+++ src/NzbDrone.Core/Restrictions/Restriction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NzbDrone.Core.Datastore;
+
+namespace NzbDrone.Core.Restrictions
+{
+    public class Restriction : ModelBase
+    {
+        public RestrictionField Field { get; set; }
+        public String Required { get; set; }
+        public String Preferred { get; set; }
+        public String Ignored { get; set; }
+        public HashSet<Int32> Tags { get; set; }
+
+        public Restriction()
+        {
+            Tags = new HashSet<Int32>();
+        }
+    }
+
+    public enum RestrictionField
+    {
+        ReleaseTitle = 0,
+        ReleaseGroup = 1
+    }
+}
---
+++ src/NzbDrone.Core/Restrictions/RestrictionService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace NzbDrone.Core.Restrictions
+{
+    public class RestrictionService
+    {
+        private readonly IRestrictionRepository _repo;
+        private readonly Logger _logger;
+
+        public RestrictionService(IRestrictionRepository repo, Logger logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        public List<Restriction> All()
+        {
+            return _repo.All().ToList();
+        }
+
+        public List<Restriction> AllForTag(Int32 tagId)
+        {
+            return _repo.All().Where(r => r.Tags.Contains(tagId)).ToList();
+        }
+
+        public List<Restriction> AllForTags(HashSet<Int32> tagIds)
+        {
+            return _repo.All().Where(r => r.Tags.Intersect(tagIds).Any()).ToList();
+        }
+    }
+}
