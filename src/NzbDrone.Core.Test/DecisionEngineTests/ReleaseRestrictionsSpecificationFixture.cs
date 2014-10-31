641c8e246cd970e8fd69f4d83fb0e1c42956034a
Mark McDowall <markus.mcd5@gmail.com>
2014-10-30 21:28:50 -0700

    Tests!

---
+++ src/NzbDrone.Core.Test/DecisionEngineTests/ReleaseRestrictionsSpecificationFixture.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.DecisionEngine.Specifications;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Restrictions;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.DecisionEngineTests
+{
+    [TestFixture]
+    public class ReleaseRestrictionsSpecificationFixture : CoreTest<ReleaseRestrictionsSpecification>
+    {
+        private RemoteEpisode _parseResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            _parseResult = new RemoteEpisode
+                           {
+                               Series = new Series
+                                        {
+                                            Tags = new HashSet<Int32>()
+                                        },
+                               Release = new ReleaseInfo
+                                         {
+                                             Title = "Dexter.S08E01.EDITED.WEBRip.x264-KYR"
+                                         }
+                           };
+        }
+
+        private void GivenRestictionRules(List<RestrictionRule> required, List<RestrictionRule> ignored)
+        {
+            Mocker.GetMock<IRestrictionService>()
+                  .Setup(s => s.AllForTags(It.IsAny<HashSet<Int32>>()))
+                  .Returns(new List<Restriction>
+                           {
+                               new Restriction
+                               {
+                                   Required = required,
+                                   Ignored = ignored
+                               }
+                           });
+        }
+
+        [Test]
+        public void should_be_true_when_restrictions_are_empty()
+        {
+            Mocker.GetMock<IRestrictionService>()
+                  .Setup(s => s.AllForTags(It.IsAny<HashSet<Int32>>()))
+                  .Returns(new List<Restriction>());
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_true_when_title_contains_all_anded_required_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule { Type = RestrictionType.And, Text = "WEBRip" },
+                                     new RestrictionRule { Type = RestrictionType.And, Text = "x264" }
+                                 },
+                                 new List<RestrictionRule>());
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_true_when_title_contains_one_ored_required_term()
+        {
+            GivenRestictionRules(new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule { Type = RestrictionType.Or, Text = "WEBRip" },
+                                     new RestrictionRule { Type = RestrictionType.Or, Text = "optional" }
+                                 },
+                                 new List<RestrictionRule>());
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_false_when_title_does_not_contain_all_anded_required_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule { Type = RestrictionType.And, Text = "WEBRip" },
+                                     new RestrictionRule { Type = RestrictionType.And, Text = "required" }
+                                 },
+                                 new List<RestrictionRule>());
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_be_false_when_title_does_not_contain_any_ored_required_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule { Type = RestrictionType.Or, Text = "opt" },
+                                     new RestrictionRule { Type = RestrictionType.Or, Text = "optional" }
+                                 },
+                                 new List<RestrictionRule>());
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_be_true_when_title_does_not_contain_all_anded_ignored_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>(),
+                                 new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule {Type = RestrictionType.And, Text = "WEBRip"},
+                                     new RestrictionRule {Type = RestrictionType.And, Text = "ignored"}
+                                 });
+                                 
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_true_when_title_does_not_contain_any_ored_ignored_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>(),
+                                 new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule {Type = RestrictionType.Or, Text = "iggy"},
+                                     new RestrictionRule {Type = RestrictionType.Or, Text = "ignored"}
+                                 });
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_false_when_title_contains_all_anded_ignored_terms()
+        {
+            GivenRestictionRules(new List<RestrictionRule>(),
+                                 new List<RestrictionRule>
+                                 {
+                                     new RestrictionRule {Type = RestrictionType.And, Text = "edited"},
+                                     new RestrictionRule {Type = RestrictionType.And, Text = "WEBRip"}
+                                 });
+
+            Subject.IsSatisfiedBy(_parseResult, null).Accepted.Should().BeFalse();
+        }
+    }
+}
--- src/NzbDrone.Core/DecisionEngine/Specifications/ReleaseRestrictionsSpecification.cs
+++ src/NzbDrone.Core/DecisionEngine/Specifications/ReleaseRestrictionsSpecification.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using NLog;
 using NzbDrone.Common;
-using NzbDrone.Core.Configuration;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Restrictions;
@@ -27,35 +26,35 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
         {
             _logger.Debug("Checking if release meets restrictions: {0}", subject);
 
+            var title = subject.Release.Title;
             var restrictions = _restrictionService.AllForTags(subject.Series.Tags);
-
             var requiredAnd = restrictions.SelectMany(r => r.Required.Where(rule => rule.Type == RestrictionType.And)).ToList();
             var requiredOr = restrictions.SelectMany(r => r.Required.Where(rule => rule.Type == RestrictionType.Or)).ToList();
             var ignoredAnd = restrictions.SelectMany(r => r.Ignored.Where(rule => rule.Type == RestrictionType.And)).ToList();
             var ignoredOr = restrictions.SelectMany(r => r.Ignored.Where(rule => rule.Type == RestrictionType.Or)).ToList();
 
-            if (requiredAnd.NotAll(rule => RuleApplies(rule, subject.Release.Title)))
+            if (requiredAnd.NotAll(rule => RuleApplies(rule, title)))
             {
-                _logger.Debug("{0} Does not contain all the required terms: {1}", subject.Release.Title, String.Join(", ", requiredAnd.Select(s => s.Text)));
-                return Decision.Reject("Does not contain all the required terms: {1}", subject.Release.Title, String.Join(", ", requiredAnd.Select(s => s.Text)));
+                _logger.Debug("{0} Does not contain all the required terms: {1}", title, String.Join(", ", requiredAnd.Select(s => s.Text)));
+                return Decision.Reject("Does not contain all the required terms: {0}", String.Join(", ", requiredAnd.Select(s => s.Text)));
             }
 
-            if (requiredOr.None(rule => RuleApplies(rule, subject.Release.Title)))
+            if (requiredOr.Any() && requiredOr.None(rule => RuleApplies(rule, title)))
             {
-                _logger.Debug("[{0}] Does not contain at least one required term: {1}", subject.Release.Title, String.Join(", ", requiredOr.Select(s => s.Text)));
-                return Decision.Reject("Does not contain at least one required term: {1}", subject.Release.Title, String.Join(", ", requiredOr.Select(s => s.Text)));
+                _logger.Debug("[{0}] Does not contain at least one required term: {1}", title, String.Join(", ", requiredOr.Select(s => s.Text)));
+                return Decision.Reject("Does not contain at least one required term: {0}", String.Join(", ", requiredOr.Select(s => s.Text)));
             }
 
-            if (ignoredAnd.All(rule => RuleApplies(rule, subject.Release.Title)))
+            if (ignoredAnd.Any() && ignoredAnd.All(rule => RuleApplies(rule, title)))
             {
-                _logger.Debug("{0} Contains all the ignored terms: {1}", subject.Release.Title, String.Join(", ", ignoredAnd.Select(s => s.Text)));
-                return Decision.Reject("Contains all the ignored terms: {1}", subject.Release.Title, String.Join(", ", ignoredAnd.Select(s => s.Text)));
+                _logger.Debug("{0} Contains all the ignored terms: {1}", title, String.Join(", ", ignoredAnd.Select(s => s.Text)));
+                return Decision.Reject("Contains all the ignored terms: {0}", String.Join(", ", ignoredAnd.Select(s => s.Text)));
             }
 
-            if (ignoredOr.Any(rule => RuleApplies(rule, subject.Release.Title)))
+            if (ignoredOr.Any(rule => RuleApplies(rule, title)))
             {
-                _logger.Debug("[{0}] Contains at least one ignored term: {1}", subject.Release.Title, String.Join(", ", ignoredOr.Select(s => s.Text)));
-                return Decision.Reject("Contains at least one ignored term: {1}", subject.Release.Title, String.Join(", ", ignoredOr.Select(s => s.Text)));
+                _logger.Debug("[{0}] Contains at least one ignored term: {1}", title, String.Join(", ", ignoredOr.Select(s => s.Text)));
+                return Decision.Reject("Contains at least one ignored term: {0}", String.Join(", ", ignoredOr.Select(s => s.Text)));
             }
 
             _logger.Debug("[{0}] No restrictions apply, allowing", subject);
